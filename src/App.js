723cb3ff3b88897d3a0ab55e5224096a98fa09cd
Mohit Khotani <mohit.khotani@gmail.com>
2020-07-21 23:30:41 +0530

    added modal

--- src/App.js
+++ src/App.js
@@ -1,24 +1,127 @@
-import React from 'react';
-import logo from './logo.svg';
+import  React, { useState }from 'react';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import Modal from '@material-ui/core/Modal';
+import Button from '@material-ui/core/Button';
+
 import './App.css';
+import data from './test.json';
+
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+}))(TableRow);
+
+function createData(id, name, tz, activity_periods) {
+  return { id, name, tz, activity_periods};
+}
+const rows = data.members.map(({ id, real_name, tz, activity_periods })  => createData(
+  id, real_name, tz, activity_periods));
+
+function rand() {
+  return Math.round(Math.random() * 20) - 10;
+}
+
+function getModalStyle() {
+  const top = 50 + rand();
+  const left = 50 + rand();
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+const useStyles = makeStyles((theme) => ({
+  paper:{
+    width: '70%',
+  },
+  paperModal: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+  const [modalStyle] = React.useState(getModalStyle);
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const body = (
+    <div style={modalStyle} className={classes.paperModal}>
+      <h3 id="title">Particular user activity</h3>
+      <p id="simple-modal-description">
+        {/* Duis mollis, est non commodo luctus, nisi erat porttitor ligula. */}
+        <Button onClick={handleClose}> Cancel</Button>
+      </p>
+    </div>
+  );
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>User Activity</h1>
       </header>
+      <TableContainer component={Paper} className = {classes.paper}>
+      <Table aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>ID</StyledTableCell>
+            <StyledTableCell align="left">Name</StyledTableCell>
+            <StyledTableCell align="left">TimeZone</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map(({id, name, tz, activity_periods}) => (
+            <StyledTableRow key={id} onClick={() => handleOpen(activity_periods)}>
+              <StyledTableCell component="th" scope="row">
+                {id}
+              </StyledTableCell>
+              <StyledTableCell align="left">{name}</StyledTableCell>
+              <StyledTableCell align="left">{tz}</StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+        {body}
+      </Modal>
     </div>
   );
 }
